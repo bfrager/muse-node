de5c60a540fc804749163f50a79a9ea94345b325
Brian Frager <bfrager@zoicstudios.com>
2016-05-20 13:51:31 -0700

    added timeline and lower navchart

--- public/js/facial-d3.js
+++ public/js/facial-d3.js
@@ -1,10 +1,10 @@
 $( function() {
 
     var margin = {
-            top: 20,
-            right: 20,
-            bottom: 30,
-            left: 50
+            top: 50,
+            right: 0,
+            bottom: 50,
+            left: 30
         },
         width = 960 - margin.left - margin.right,
         height = 600 - margin.top - margin.bottom;
@@ -16,6 +16,13 @@ $( function() {
         .range( [height, 0] );
 
     var color = d3.scale.category10();
+
+    // var xScale = d3.scale.linear()
+    //     .domain([0, 250000])
+    //     .range([0, width]),
+    //     yScale = d3.scale.linear()
+    //     .domain([0, 100]).nice()
+    //     .range([height, 0]);
 
     var xAxis = d3.svg.axis()
         .scale(x)
@@ -34,7 +41,8 @@ $( function() {
             return y( d.confidence );
         } );
 
-    var svg = d3.select( "body" ).append( "div" ).append( "svg" )
+    var svg = d3.select( "body" ).append( "div" ).classed('chart', true).append( "svg" )
+        .classed('mainChart', true)
         .attr( "width", width + margin.left + margin.right )
         .attr( "height", height + margin.top + margin.bottom )
         // .style( "margin-left", margin.left + "px" )
@@ -116,27 +124,39 @@ $( function() {
           .attr("data-legend",function(d) { return d.name.slice(0, -27)})
           .style("stroke", function(d) { return color(d.name); });
 
-      legendSpace = width / emotions.length; // spacing for legend // ******
+      // timeline
+      var timeLine = svg.append("rect")
+          .classed("timed", true)
+          .attr("x", width / 2)
+          .attr("y", 0)
+          .attr("width", 5)
+          .attr("height", height)
+          .style("fill", "blue");
 
-      emotion.append("text")
-        .attr("x", function(d, i) { return (legendSpace / 2) + i * legendSpace })
-        .attr("y", margin.top / 2)
-        .attr("class", "legend")
-        .style("fill", function(d) {
-            return d.color = color(d.name); })
-        .on("click", function(d){                     // ************
-            // Determine if current line is visible
-            var active   = d.active ? false : true,  // ************
-            newOpacity = active ? 0 : 1;             // ************
-            // Hide or show the elements based on the ID
-            d3.select("#tag"+d.name.replace(/\s+/g, '')) // *********
-                .transition().duration(100)          // ************
-                .style("opacity", newOpacity);       // ************
-            // Update whether or not the elements are active
-            d.active = active;                       // ************
-            })                                       // ************
-        .text(function(d) { return d.name.slice(0, -27) } );
 
+      // // Top Legend
+      // legendSpace = width / emotions.length; // spacing for legend // ******
+      //
+      // emotion.append("text")
+      //   .attr("x", function(d, i) { return (legendSpace / 2) + i * legendSpace })
+      //   .attr("y", margin.top / 2)
+      //   .attr("class", "legend")
+      //   .style("fill", function(d) {
+      //       return d.color = color(d.name); })
+      //   .on("click", function(d){                     // ************
+      //       // Determine if current line is visible
+      //       var active   = d.active ? false : true,  // ************
+      //       newOpacity = active ? 0 : 1;             // ************
+      //       // Hide or show the elements based on the ID
+      //       d3.select("#tag"+d.name.replace(/\s+/g, '')) // *********
+      //           .transition().duration(100)          // ************
+      //           .style("opacity", newOpacity);       // ************
+      //       // Update whether or not the elements are active
+      //       d.active = active;                       // ************
+      //       })                                       // ************
+      //   .text(function(d) { return d.name.slice(0, -27) } );
+
+      // Sidebar Legend
       legend = svg.append("g")
           .attr("class","legend")
           .attr("transform","translate(50,30)")
@@ -144,6 +164,64 @@ $( function() {
           .attr("data-style-padding",10)
           .call(d3.legend);
 
+      //Lower chart
+      var navWidth = width,
+          navHeight = 100 - margin.top - margin.bottom;
+
+      var navChart = d3.select( "body" ).append( "div" ).classed('chart', true).append('svg')
+          .classed('navigator', true)
+          .attr('width', navWidth + margin.left + margin.right)
+          .attr('height', navHeight + margin.top + margin.bottom)
+          .append('g')
+          .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+      var navXScale = d3.scale.linear()
+              .domain([0, 250000])
+              .range([0, navWidth]),
+          navYScale = d3.scale.linear()
+              .domain([0, 100])
+              .range([navHeight, 0]);
+
+      var navXAxis = d3.svg.axis()
+          .scale(navXScale)
+          .orient('bottom');
+
+      navChart.append('g')
+          .attr('class', 'x axis')
+          .attr('transform', 'translate(0,' + navHeight + ')')
+          .call(navXAxis);
+
+      var emotion = navChart.selectAll(".emotion")
+          .data(emotions)
+          .enter().append("g")
+          .attr("class", "emotion");
+
+      emotion.append("path")
+          .attr("class", "line")
+          .attr("d", function(d) {
+            return line(d.values);
+          })
+          .style("stroke", function(d) { return color(d.name); });
+
+      var navData = d3.svg.area()
+          .x(function (d) { return navXScale(d.date); })
+          .y0(navHeight)
+          .y1(function (d) { return navYScale(d.close); });
+
+      var navLine = d3.svg.line()
+          .x(function (d) { return navXScale(d.date); })
+          .y(function (d) { return navYScale(d.close); });
+
+      navChart.append('path')
+          .attr('class', 'data')
+          .attr('d', navData(data));
+
+      navChart.append('path')
+          .attr('class', 'line')
+          .attr('d', navLine(data));
+
     });
+
+
 
 } );
