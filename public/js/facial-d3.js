d7d3932a463fa986895ce5f5a08f3e64b5876347
Brian Frager <bfrager@zoicstudios.com>
2016-05-19 18:53:47 -0700

    added alternate legend, debugging interactivity

--- public/js/facial-d3.js
+++ public/js/facial-d3.js
@@ -1,221 +1,149 @@
 $( function() {
 
-    // d3.csv("../assets/data/IMG_6135facialcapture.csv")
-    //   .row(function(d) { return {key: d.key, value: +d.value}; })
-    //   .get(function(error, rows) { console.log(rows); });
-    //
-    // var n = 40,
-    //     random = d3.random.normal( 800, 1000 );
-    //
-    // function chart(domain, interpolation, tick) {
-    //     var data = [d3.range( n ).map( random ), d3.range( n ).map( random ), d3.range( n ).map( random ), d3.range( n ).map( random )];
+    var margin = {
+            top: 20,
+            right: 20,
+            bottom: 30,
+            left: 50
+        },
+        width = 960 - margin.left - margin.right,
+        height = 600 - margin.top - margin.bottom;
 
-        var margin = {
-                top: 20,
-                right: 20,
-                bottom: 30,
-                left: 50
-            },
-            width = 960 - margin.left - margin.right,
-            height = 600 - margin.top - margin.bottom;
+    var x = d3.scale.linear()
+        .range( [0, width] );
 
-        var x = d3.scale.linear()
-            .range( [0, width] );
+    var y = d3.scale.linear()
+        .range( [height, 0] );
 
-        var y = d3.scale.linear()
-            .range( [height, 0] );
+    var color = d3.scale.category10();
 
-        var color = d3.scale.category10();
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom");
 
-        var xAxis = d3.svg.axis()
-            .scale(x)
-            .orient("bottom");
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left");
 
-        var yAxis = d3.svg.axis()
-            .scale(y)
-            .orient("left");
+    var line = d3.svg.line()
+        .interpolate("basis")
+        .x( function(d) {
+            return x( d.timestamp );
+        } )
+        .y( function(d) {
+            return y( d.confidence );
+        } );
 
-        var line = d3.svg.line()
-            .interpolate("basis")
-            .x( function(d) {
-                return x( d.timestamp );
-            } )
-            .y( function(d) {
-                return y( d.confidence );
-            } );
+    var svg = d3.select( "body" ).append( "div" ).append( "svg" )
+        .attr( "width", width + margin.left + margin.right )
+        .attr( "height", height + margin.top + margin.bottom )
+        // .style( "margin-left", margin.left + "px" )
+        // .style( "margin-top", margin.top + "px" )
+        .append( "g" )
+        .attr( "transform", "translate(" + margin.left + "," + margin.top + ")" );
 
-        var svg = d3.select( "body" ).append( "div" ).append( "svg" )
-            .attr( "width", width + margin.left + margin.right )
-            .attr( "height", height + margin.top + margin.bottom )
-            // .style( "margin-left", margin.left + "px" )
-            // .style( "margin-top", margin.top + "px" )
-            .append( "g" )
-            .attr( "transform", "translate(" + margin.left + "," + margin.top + ")" );
+    d3.csv("../assets/data/IMG_6135facialcapture_clean.csv", function(error, data) {
+      if (error) throw error;
 
-        d3.csv("../assets/data/IMG_6135facialcapture_clean.csv", function(error, data) {
-          if (error) throw error;
+      // data.forEach(function(d) {
+      //   Object.keys(d).forEach(function(key){
+      //     d[key] = +d[key];
+      //   });
+      // });
 
-          // data.forEach(function(d) {
-          //   Object.keys(d).forEach(function(key){
-          //     d[key] = +d[key];
-          //   });
-          // });
+      var emotionsRef = [
+        "joyct_emotion_nonlinear_causal",
+        "disgustct_emotion_nonlinear_causal",
+        "sadnessct_emotion_nonlinear_causal",
+        "angerct_emotion_nonlinear_causal",
+        "surprisect_emotion_nonlinear_causal",
+        "contemptct_emotion_nonlinear_causal",
+        "fearct_emotion_nonlinear_causal"
+      ];
 
+      // color.domain(d3.keys(data[0]).filter(function(key) { return key !== "timestamp"; }));
+      color.domain(d3.keys(data[0]).filter(function(key) { return emotionsRef.includes(key) }));
 
-          var emotionsRef = [
-            "joyct_emotion_nonlinear_causal",
-            "disgustct_emotion_nonlinear_causal",
-            "sadnessct_emotion_nonlinear_causal",
-            "angerct_emotion_nonlinear_causal",
-            "surprisect_emotion_nonlinear_causal",
-            "contemptct_emotion_nonlinear_causal",
-            "fearct_emotion_nonlinear_causal"
-          ];
+      var emotions = color.domain().map(function(name) {
+        return {
+          name: name,
+          values: data.map(function(d) {
+            return {timestamp: +d.timestamp, confidence: +d[name]};
+          })
+        };
+      });
 
-          // color.domain(d3.keys(data[0]).filter(function(key) { return key !== "timestamp"; }));
-          color.domain(d3.keys(data[0]).filter(function(key) { return emotionsRef.includes(key) }));
+      // x.domain(d3.extent(data, function(d) { return d.timestamp; }));
+      x.domain([0, 250000]);
 
-          var emotions = color.domain().map(function(name) {
-            return {
-              name: name,
-              values: data.map(function(d) {
-                return {timestamp: +d.timestamp, confidence: +d[name]};
-              })
-            };
-          });
+      y.domain([
+        0,
+        100
+        // d3.max(data, function(d) { return d.joyct_emotion_nonlinear_causal; })
+      ]);
 
-          // x.domain(d3.extent(data, function(d) { return d.timestamp; }));
-          x.domain([0, 250000]);
+      svg.append("g")
+          .attr("class", "x axis")
+          .attr("transform", "translate(0," + height + ")")
+          .call(xAxis)
+          .append("text")
+          .attr("x", width / 2)
+          .attr("y", 30)
+          .attr("dx", ".71em")
+          .style("text-anchor", "end")
+          .text("Milliseconds");
 
-          y.domain([
-            0,
-            100
-            // d3.max(data, function(d) { return d.joyct_emotion_nonlinear_causal; })
-          ]);
+      svg.append("g")
+          .attr("class", "y axis")
+          .call(yAxis)
+          .append("text")
+          .attr("transform", "rotate(-90)")
+          .attr("y", 6)
+          .attr("dy", ".71em")
+          .style("text-anchor", "end")
+          .text("Intensity");
 
-          svg.append("g")
-              .attr("class", "x axis")
-              .attr("transform", "translate(0," + height + ")")
-              .call(xAxis)
-              .append("text")
-              .attr("x", width / 2)
-              .attr("y", 30)
-              .attr("dx", ".71em")
-              .style("text-anchor", "end")
-              .text("Milliseconds");
+      var emotion = svg.selectAll(".emotion")
+          .data(emotions)
+          .enter().append("g")
+          .attr("class", "emotion");
 
-          svg.append("g")
-              .attr("class", "y axis")
-              .call(yAxis)
-              .append("text")
-              .attr("transform", "rotate(-90)")
-              .attr("y", 6)
-              .attr("dy", ".71em")
-              .style("text-anchor", "end")
-              .text("Intensity");
+      emotion.append("path")
+          .attr("class", "line")
+          .attr("d", function(d) {
+            return line(d.values);
+          })
+          .attr("data-legend",function(d) { return d.name.slice(0, -27)})
+          .style("stroke", function(d) { return color(d.name); });
 
-          var emotion = svg.selectAll(".emotion")
-              .data(emotions)
-              .enter().append("g")
-              .attr("class", "emotion");
+      legendSpace = width / emotions.length; // spacing for legend // ******
 
-          emotion.append("path")
-              .attr("class", "line")
-              .attr("d", function(d) {
-                return line(d.values);
-              })
-              .attr("data-legend",function(d) { return d.name.slice(0, -27)})
-              .style("stroke", function(d) { return color(d.name); });
+      emotion.append("text")
+        .attr("x", function(d, i) { return (legendSpace / 2) + i * legendSpace })
+        .attr("y", margin.top / 2)
+        .attr("class", "legend")
+        .style("fill", function(d) {
+            return d.color = color(d.name); })
+        .on("click", function(d){                     // ************
+            // Determine if current line is visible
+            var active   = d.active ? false : true,  // ************
+            newOpacity = active ? 0 : 1;             // ************
+            // Hide or show the elements based on the ID
+            d3.select("#tag"+d.name.replace(/\s+/g, '')) // *********
+                .transition().duration(100)          // ************
+                .style("opacity", newOpacity);       // ************
+            // Update whether or not the elements are active
+            d.active = active;                       // ************
+            })                                       // ************
+        .text(function(d) { return d.name.slice(0, -27) } );
 
-          emotion.append("text")
-              .datum(function(d) { return {name: d.name, value: d.values[d.values.length - 1]}; })
-              .attr("transform", function(d) { return "translate(" + x(d.value.timestamp) + "," + y(d.value.confidence) + ")"; })
-              .attr("x", 3)
-              .attr("dy", ".35em")
-              .text(function(d) { return d.name; });
+      legend = svg.append("g")
+          .attr("class","legend")
+          .attr("transform","translate(50,30)")
+          .style("font-size","16px")
+          .attr("data-style-padding",10)
+          .call(d3.legend);
 
-          legend = svg.append("g")
-              .attr("class","legend")
-              .attr("transform","translate(50,30)")
-              .style("font-size","12px")
-              .call(d3.legend)
-
-            setTimeout(function() {
-              legend
-                .style("font-size","20px")
-                .attr("data-style-padding",10)
-                .call(d3.legend)
-            },1000)
-
-        });
-
-
-    //
-    //     var lines = svg.selectAll( "g" )
-    //         .data( data );
-    //
-    //     var aLineContainer = lines
-    //         .enter().append( "g" );
-    //
-    //     aLineContainer.append( "defs" ).append( "clipPath" )
-    //         .attr( "id", "clip" )
-    //         .append( "rect" )
-    //         .attr( "width", width )
-    //         .attr( "height", height );
-    //
-    //     svg.append( "g" )
-    //         .attr( "class", "y axis" )
-    //         .call( d3.svg.axis().scale( y ).ticks( 5 ).orient( "left" ) )
-    //         .append( "text" )
-    //         .attr( "transform", "rotate(-90)" )
-    //         .attr( "y", 6 )
-    //         .attr( "dy", ".71em" )
-    //         .style( "text-anchor", "end" )
-    //         .text( "microvolts" );
-    //
-    //     var colors = ["red", "#92278F", "#0071BB", "#00A651"];
-    //
-    //     var path = aLineContainer
-    //         .attr( "clip-path", "url(#clip)" )
-    //         .append( "path" )
-    //         //.data(data)
-    //         .attr( "class", "line" )
-    //         .style( "stroke", function(d, i) {
-    //             return colors[i];
-    //         } )
-    //         .attr( "d", line );
-    //
-    //     // tick(path, line, data, x);
-    //
-    //     var socket = io.connect( "http://localhost:3000" );
-    //     socket.on( "news", function(value) {
-    //         if (!(value.args && value.address == "/muse/eeg"))
-    //             return;
-    //
-    //         var xVal = (new Date()).getTime(), // current time
-    //             yVal = value.args[0];
-    //
-    //         data[0].push( value.args[0] );
-    //         data[1].push( value.args[1] );
-    //         data[2].push( value.args[2] );
-    //         data[3].push( value.args[3] );
-    //
-    //         path
-    //             .attr( "d", line )
-    //             .attr( "transform", null )
-    //             .transition()
-    //             .duration( 750 )
-    //             .ease( "linear" )
-    //             .attr( "transform", "translate(" + x( 0 ) + ")" );
-    //
-    //         // pop the old data point off the front
-    //         data[0].shift();
-    //         data[1].shift();
-    //         data[2].shift();
-    //         data[3].shift();
-    //     } );
-    // }
-    //
-    // chart( [1, n - 2], "basis", function tick(path, line, data, x) {} );
+    });
 
 } );
